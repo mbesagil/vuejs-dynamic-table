3c473b75b2484c5330d8c326a7e370124095777d
mbesagil <m.besagil@gmail.com>
2023-02-09 09:45:13 +0300

    dynamic table working responsive

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container my-3">
+    <table class="table">
+      <tr v-for="(data, k) in keyValueData" :key="k">
+        <td>
+          <input
+            placeholder="Key"
+            class="form-control"
+            type="text"
+            v-model="data.key"
+          />
+        </td>
+        <td>
+          <input
+            placeholder="Value"
+            class="form-control"
+            type="text"
+            v-model="data.value"
+          />
+        </td>
+        <td scope="row" class="trashIconContainer">
+          <i class="far fa-trash-alt" @click="deleteRow(k, data)"></i>
+        </td>
+      </tr>
+    </table>
+    <div>
+      <button type="button" class="btn btn-info" @click="addNewRow">
+        <i class="fas fa-plus-circle"></i>
+        Add
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      keyValueData: [
+        {
+          // key: null,
+          // value: null,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    addNewRow() {
+      console.log("this.keyValueData", this.keyValueData);
+
+      this.keyValueData.push({
+        key: "",
+        value: "",
+      });
+    },
+    deleteRow(index, data) {
+      var idx = this.keyValueData.indexOf(data);
+      console.log(idx, index);
+      if (idx > -1) {
+        this.keyValueData.splice(idx, 1);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
